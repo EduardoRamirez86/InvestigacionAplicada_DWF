de2eb7b460249bce169d7de75d1a3f857cda7500
EduardoRamirez86 <eduardoalfredo124@gmail.com>
2025-04-05 21:29:02 -0600

    proyecto

---
+++ InvestigacionDwf/src/test/java/sv/edu/udb/InvestigacionDwf/InvestigacionDwfApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.InvestigacionDwf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestigacionDwfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
